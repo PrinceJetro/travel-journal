0d8525cff5d33de8f2f180a46340717d58af0cdc
PrinceJetro <adegbuyijephthah@gmail.com>
2024-04-05 12:14:11 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from "react";
-import { ReactDOM } from "react";
-import Navbar from './components/navbar'
-import Card from './components/cards'
-import data from "./components/data"
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Complaint from './components/complaint';
+import ComplaintPageList from "./components/displayComplaints"
 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-const mainData = data.map(items =>{
-  return(
-    <Card
-    key={items.image}
-    item={items}
-    />
-  )
-}) 
   return (
-    <div className="container">
-      <Navbar/>
-      {mainData}
-      
-    </div>
+    <>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/complaint" element={<Complaint />} />
+        <Route path="/display" element={<ComplaintPageList />} />
+
+       {/*  <Route path="/movie/:src" element={<MovieDetails />} />
+        <Route path="/watchnow/:title" element={<Movie />} /> */}
+
+      </Routes>
+    </Router>
+    <Footer/>
+    </>
   );
 }
 
